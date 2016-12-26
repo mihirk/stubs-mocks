5737031d128a286ea66f1220631d3b597111e9b7
mihirk <mihir.khatwani@gmail.com>
2016-12-26 11:50:37 +0530

    Removing

--- src/test/java/org/egov/bootcamp/service/EntityServiceTest.java
+++ src/test/java/org/egov/bootcamp/service/EntityServiceTest.java
@@ -1,5 +1,25 @@
 package org.egov.bootcamp.service;
 
+import org.egov.bootcamp.dao.EntityDao;
+import org.junit.Before;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.when;
+
 public class EntityServiceTest {
+
+    @Mock
+    private EntityDao entityDao;
+
+    @InjectMocks
+    private EntityService entityService;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        when(entityDao.get(0)).thenReturn(null);
+    }
 
 }
